d0bed48f1a23a1363e01c6b074d4096bc1051d67
Ivo Stole Junior <stolejunior@hotmail.com>
2023-01-20 14:00:00 -0300

    comando delete

--- index.php
+++ index.php
@@ -27,11 +27,21 @@ $aluno->insert();
 echo $aluno;
 */
 
+/*Alterar um usuário
 $usuario = new Usuario();
 
 $usuario->loadById(8);
 
 $usuario->update("professor", "!@#$%¨&*");
+
+echo $usuario;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(7);
+
+$usuario->delete();
 
 echo $usuario;
 
